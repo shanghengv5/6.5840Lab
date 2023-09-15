55a564d9139818000d79376d0f02d3f721b17ef7
lishangheng <394918428@qq.com>
2023-09-15 16:54:43 +0800

    feat:lab1 pass test

--- src/mr/coordinator.go
+++ src/mr/coordinator.go
@@ -1,33 +1,116 @@
 package mr
 
-import "log"
-import "net"
-import "os"
-import "net/rpc"
-import "net/http"
+import (
+	"fmt"
+	"log"
+	"net"
+	"net/http"
+	"net/rpc"
+	"os"
+	"sync"
+)
 
+type WorkStatus int
 
 type Coordinator struct {
 	// Your definitions here.
+	files   []string
+	nReduce int
 
+	jobTypeStatus map[JobType]WorkStatus
+	jobs          map[int]WorkStatus
+	reduceJobs    map[int]WorkStatus
+
+	mu sync.Mutex
+
+	Retry map[JobType]map[int]int
 }
 
-// Your code here -- RPC handlers for the worker to call.
+// Worker ask job to do
+func (c *Coordinator) Work(args *WorkArgs, reply *WorkReply) error {
+	c.mu.Lock()
+	defer c.mu.Unlock()
+	// if worker call work,we think it still alive
 
-//
-// an example RPC handler.
-//
-// the RPC argument and reply types are defined in rpc.go.
-//
-func (c *Coordinator) Example(args *ExampleArgs, reply *ExampleReply) error {
-	reply.Y = args.X + 1
+	if args.JobType == mapJob {
+		for n, b := range c.jobs {
+			if b == idle {
+				reply.Files = append(reply.Files, c.files[n])
+				reply.NReduce = c.nReduce
+				reply.JobNum = n
+				c.Retry[args.JobType][n] = 0
+				c.jobs[n] = work
+				return nil
+			}
+		}
+	} else if args.JobType == reduceJob {
+		reply.JobNum = c.nReduce
+		reply.NReduce = c.nReduce
+		for n, b := range c.reduceJobs {
+			if b == idle {
+				c.reduceJobs[n] = work
+				reply.JobNum = n
+				c.Retry[args.JobType][n] = 0
+				return nil
+			}
+		}
+	}
+
 	return nil
 }
 
+func (c *Coordinator) Log() {
+	fmt.Println("Retrys:", c.Retry)
 
-//
+}
+
+// Worker reply coordinator one job is done
+func (c *Coordinator) WorkDone(args *WorkDoneArgs, reply *WorkDoneReply) error {
+	c.mu.Lock()
+	defer c.mu.Unlock()
+	// if worker call  done work,we think it still alive
+
+	if args.JobType == mapJob {
+		if c.jobs[args.JobNum] != done {
+			c.jobs[args.JobNum] = done
+		}
+
+		// Finish all map job
+		isDone := true
+		for _, b := range c.jobs {
+			if b != done {
+				isDone = false
+			}
+		}
+		if isDone {
+			c.jobTypeStatus[mapJob] = done
+			c.jobTypeStatus[reduceJob] = work
+			reply.StepDone = true
+		}
+	} else if args.JobType == reduceJob && c.jobTypeStatus[mapJob] == done {
+		if c.reduceJobs[args.JobNum] != done {
+			c.reduceJobs[args.JobNum] = done
+		}
+
+		isDone := true
+		for _, b := range c.reduceJobs {
+			if b != done {
+				isDone = false
+			}
+		}
+
+		if isDone {
+			c.jobTypeStatus[reduceJob] = done
+			reply.StepDone = true
+		}
+		// all done
+		c.Done()
+	}
+
+	return nil
+}
+
 // start a thread that listens for RPCs from worker.go
-//
 func (c *Coordinator) server() {
 	rpc.Register(c)
 	rpc.HandleHTTP()
@@ -41,29 +124,64 @@ func (c *Coordinator) server() {
 	go http.Serve(l, nil)
 }
 
-//
 // main/mrcoordinator.go calls Done() periodically to find out
 // if the entire job has finished.
-//
 func (c *Coordinator) Done() bool {
 	ret := false
-
 	// Your code here.
-
+	if c.jobTypeStatus[mapJob] == done && c.jobTypeStatus[reduceJob] == done {
+		return true
+	}
+	c.Log()
+	//reset
+	for job, b := range c.jobs {
+		if b == work {
+			c.Retry[mapJob][job]++
+			if c.Retry[mapJob][job] > 10 {
+				c.jobs[job] = idle
+				c.Retry[mapJob][job] = 0
+			}
+		}
+	}
+	for job, b := range c.reduceJobs {
+		if b == work {
+			c.Retry[reduceJob][job]++
+			if c.Retry[reduceJob][job] > 10 {
+				c.reduceJobs[job] = idle
+				c.Retry[reduceJob][job] = 0
+			}
+		}
+	}
 
 	return ret
 }
 
-//
 // create a Coordinator.
 // main/mrcoordinator.go calls this function.
 // nReduce is the number of reduce tasks to use.
-//
 func MakeCoordinator(files []string, nReduce int) *Coordinator {
-	c := Coordinator{}
+	c := Coordinator{
+		jobs:          make(map[int]WorkStatus),
+		jobTypeStatus: make(map[JobType]WorkStatus),
+		reduceJobs:    make(map[int]WorkStatus),
+		Retry:         make(map[JobType]map[int]int),
+	}
 
-	// Your code here.
+	c.files = files
+	c.nReduce = nReduce
+	// init
+	for i := 0; i < len(c.files); i++ {
+		c.jobs[i] = idle
+	}
 
+	for i := 0; i < nReduce; i++ {
+		c.reduceJobs[i] = idle
+	}
+	c.Retry[mapJob] = make(map[int]int)
+	c.Retry[reduceJob] = make(map[int]int)
+	c.jobTypeStatus[mapJob] = work
+	c.jobTypeStatus[intermediateJob] = idle
+	c.jobTypeStatus[reduceJob] = idle
 
 	c.server()
 	return &c
--- src/mr/worker.go
+++ src/mr/worker.go
@@ -1,77 +1,194 @@
 package mr
 
-import "fmt"
-import "log"
-import "net/rpc"
-import "hash/fnv"
+import (
+	"encoding/json"
+	"errors"
+	"fmt"
+	"hash/fnv"
+	"io"
+	"log"
+	"net/rpc"
+	"os"
+	"regexp"
+	"sort"
+)
 
-
-//
 // Map functions return a slice of KeyValue.
-//
 type KeyValue struct {
 	Key   string
 	Value string
 }
 
-//
+type ByKey []KeyValue
+
+func (a ByKey) Len() int           { return len(a) }
+func (a ByKey) Swap(i, j int)      { a[i], a[j] = a[j], a[i] }
+func (a ByKey) Less(i, j int) bool { return a[i].Key < a[j].Key }
+
 // use ihash(key) % NReduce to choose the reduce
 // task number for each KeyValue emitted by Map.
-//
 func ihash(key string) int {
 	h := fnv.New32a()
 	h.Write([]byte(key))
 	return int(h.Sum32() & 0x7fffffff)
 }
 
-
-//
 // main/mrworker.go calls this function.
-//
 func Worker(mapf func(string, string) []KeyValue,
 	reducef func(string, []string) string) {
 
 	// Your worker implementation here.
+	// Ask a map job
+	for err := mapWork(mapf); err != nil; err = mapWork(mapf) {
 
-	// uncomment to send the Example RPC to the coordinator.
-	// CallExample()
+	}
+	// Ask a reduce job
+	for err := reduceWork(reducef); err != nil; err = reduceWork(reducef) {
 
-}
-
-//
-// example function to show how to make an RPC call to the coordinator.
-//
-// the RPC argument and reply types are defined in rpc.go.
-//
-func CallExample() {
-
-	// declare an argument structure.
-	args := ExampleArgs{}
-
-	// fill in the argument(s).
-	args.X = 99
-
-	// declare a reply structure.
-	reply := ExampleReply{}
-
-	// send the RPC request, wait for the reply.
-	// the "Coordinator.Example" tells the
-	// receiving server that we'd like to call
-	// the Example() method of struct Coordinator.
-	ok := call("Coordinator.Example", &args, &reply)
-	if ok {
-		// reply.Y should be 100.
-		fmt.Printf("reply.Y %v\n", reply.Y)
-	} else {
-		fmt.Printf("call failed!\n")
 	}
 }
 
-//
+func reduceWork(reducef func(string, []string) string) error {
+	reply := WorkReply{}
+	arg := WorkArgs{JobType: reduceJob}
+	ok := call("Coordinator.Work", &arg, &reply)
+	if ok && reply.JobNum != reply.NReduce {
+		kva := []KeyValue{}
+		jobNum := reply.JobNum
+		// Find intermediate
+		curDir, err := os.Getwd()
+		if err != nil {
+			return err
+		}
+		files, err := os.ReadDir(curDir)
+		if err != nil {
+			return err
+		}
+		regStr := fmt.Sprintf(`^mr-%d-.+$`, jobNum)
+		regex := regexp.MustCompile(regStr)
+
+		for _, file := range files {
+			if regex.MatchString(file.Name()) {
+				f, err := os.Open(file.Name())
+				if err != nil {
+					return err
+				}
+				dec := json.NewDecoder(f)
+				for {
+					var kv KeyValue
+					if err := dec.Decode(&kv); err != nil {
+						break
+					}
+					kva = append(kva, kv)
+				}
+			}
+		}
+		// sort Intermediate
+		sort.Sort(ByKey(kva))
+		path := fmt.Sprintf("mr-out-%d", jobNum)
+
+		tmpFile, err := os.Create(path)
+		if err != nil {
+			return fmt.Errorf("create tmp file failed!\n")
+		}
+		for i := 0; i < len(kva); {
+			j := i + 1
+			for j < len(kva) && kva[i].Key == kva[j].Key {
+				j++
+			}
+			values := []string{}
+			for k := i; k < j; k++ {
+				values = append(values, kva[k].Value)
+			}
+			output := reducef(kva[i].Key, values)
+			_, err = fmt.Fprintf(tmpFile, "%v %v\n", kva[i].Key, output)
+			if err != nil {
+				return err
+			}
+			i = j
+		}
+
+		if err = tmpFile.Close(); err != nil {
+			return err
+		}
+		doneReply := WorkDoneReply{}
+		doneArgs := WorkDoneArgs{JobType: reduceJob, JobNum: reply.JobNum}
+
+		ok = call("Coordinator.WorkDone", &doneArgs, &doneReply)
+
+		if ok && doneReply.StepDone {
+			return nil
+		}
+	}
+
+	return errors.New("need a new reduce work")
+}
+
+// A map work
+func mapWork(mapf func(string, string) []KeyValue) error {
+	reply := WorkReply{}
+	arg := WorkArgs{JobType: mapJob}
+	ok := call("Coordinator.Work", &arg, &reply)
+	if ok {
+		// Map work
+		intermediate := make([][]KeyValue, reply.NReduce)
+
+		for _, fileName := range reply.Files {
+			file, err := os.Open(fileName)
+			if err != nil {
+				return err
+			}
+			contents, err := io.ReadAll(file)
+			if err != nil {
+				return err
+			}
+			kv := mapf(fileName, string(contents))
+			// Create intermediate
+			for _, kv := range kv {
+				k := ihash(kv.Key) % reply.NReduce
+				intermediate[k] = append(intermediate[k], kv)
+			}
+
+		}
+		// save intermediate
+		for idx, kv := range intermediate {
+			path := fmt.Sprintf("mr-%d-%d", idx, reply.JobNum)
+
+			tmpFile, err := os.Create(path)
+			if err != nil {
+				return err
+			}
+			enc := json.NewEncoder(tmpFile)
+			if err != nil {
+				return err
+			}
+			for _, kv := range kv {
+				err = enc.Encode(&kv)
+				if err != nil {
+					return err
+				}
+			}
+			err = tmpFile.Close()
+			if err != nil {
+				return err
+			}
+		}
+
+		doneReply := WorkDoneReply{}
+		doneArgs := WorkDoneArgs{JobType: mapJob, JobNum: reply.JobNum}
+		ok = call("Coordinator.WorkDone", &doneArgs, &doneReply)
+
+		if ok && doneReply.StepDone {
+			return nil
+		}
+	}
+
+	return errors.New("need a new map work")
+}
+
 // send an RPC request to the coordinator, wait for the response.
 // usually returns true.
 // returns false if something goes wrong.
-//
 func call(rpcname string, args interface{}, reply interface{}) bool {
 	// c, err := rpc.DialHTTP("tcp", "127.0.0.1"+":1234")
 	sockname := coordinatorSock()
